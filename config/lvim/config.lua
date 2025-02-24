70a432ffe2ab5c1a7fc902f72525d3c4887c25a6
Rodrigo Tavares <rodr1gowzxz2@gmail.com>
2025-02-24 19:11:22 -0300

    Update config.lua

--- config/lvim/config.lua
+++ config/lvim/config.lua
@@ -6,16 +6,30 @@
 
 
 lvim.plugins = {
+  -- Themes
     {
         "catppuccin/nvim",
         name = "catppuccin", -- Nome do tema (necessário para catppuccin)
         config = function()
             require("catppuccin").setup({
                 flavour = "mocha", -- Estilos disponíveis: latte, frappe, macchiato, mocha
-                transparent_background = false,
+                transparent_background = true,
             })
         end,
     },
+    
+    {
+        "ellisonleao/gruvbox.nvim",
+        name = "gruvbox",
+        config = function()
+            require("gruvbox").setup({
+                contrast = "hard", -- Opções: "soft", "medium", "hard"
+                transparent_mode = true, -- Fundo transparente
+            })
+        end,
+    },
+    
+    -- Addons
     {
         "brenoprata10/nvim-highlight-colors",
         config = function()
@@ -31,4 +45,32 @@ lvim.plugins = {
 
 ---------------------------Styles ----------------------------
 lvim.transparent_window = true
-lvim.colorscheme = "catppuccin"
+
+--lvim.colorscheme = "catppuccin"
+--lvim.colorscheme = "hebamax"
+--lvim.colorscheme = "gruvbox"
+lvim.colorscheme = "catppuccin-mocha"
+--lvim.colorscheme = "default"
+--lvim.colorscheme = "blue"
+
+-- Remover fundo de todos os elementos
+vim.cmd [[
+    highlight Normal guibg=NONE ctermbg=NONE
+    highlight NormalNC guibg=NONE ctermbg=NONE
+    highlight SignColumn guibg=NONE ctermbg=NONE
+    highlight StatusLine guibg=NONE ctermbg=NONE
+    highlight StatusLineNC guibg=NONE ctermbg=NONE
+    highlight LineNr guibg=NONE ctermbg=NONE
+    highlight NonText guibg=NONE ctermbg=NONE
+    highlight EndOfBuffer guibg=NONE ctermbg=NONE
+    highlight VertSplit guibg=NONE ctermbg=NONE
+    highlight TabLine guibg=NONE ctermbg=NONE
+    highlight TabLineFill guibg=NONE ctermbg=NONE
+    highlight TabLineSel guibg=NONE ctermbg=NONE
+    highlight Pmenu guibg=NONE ctermbg=NONE
+    highlight PmenuSel guibg=NONE ctermbg=NONE
+    highlight PmenuSbar guibg=NONE ctermbg=NONE
+    highlight PmenuThumb guibg=NONE ctermbg=NONE
+    highlight FloatBorder guibg=NONE ctermbg=NONE
+    highlight NormalFloat guibg=NONE ctermbg=NONE
+]]
